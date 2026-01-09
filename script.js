fee632ef57e9b192da9238cc4c97f8114202c0e9
Akshit Garg <akshitg9581@gmail.com>
2026-01-09 17:58:57 +0530

    Intial Commit

---
+++ script.js
@@ -0,0 +1,56 @@
+console.log("Welcome to Tic Tac Toe");
+let music = new Audio("music.mp3");
+let audioturn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X";
+let isgameover = false;
+
+//Function to change the turn
+const changeTurn = () => {
+  return turn === "X" ? "O" : "X";
+};
+
+//Function to check for a win
+const checkWin = () => {
+  let boxtests = document.getElementsByClassName("boxtext");
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  wins.forEach((e) => {
+    if (
+      boxtests[e[0]].innerText === boxtests[e[1]].innerText &&
+      boxtests[e[2]].innerText === boxtests[e[1]].innerText &&
+      boxtests[e[0]].innerText !== ""
+    ) {
+      document.querySelector(".Info").innerText =
+        boxtests[e[0]].innerText + " Won";
+      isgameover = true;
+    }
+  });
+};
+
+//Game Logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turn;
+      turn = changeTurn();
+      audioturn.currentTime = 0;
+      audioturn.play();
+      checkWin();
+      if (!isgameover) {
+        document.getElementsByClassName("Info")[0].innerText =
+          "Turn for " + turn;
+      }
+    }
+  });
+});
